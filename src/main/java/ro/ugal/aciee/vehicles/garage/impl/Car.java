d3f30b2b46aaeec7a6c675dd717599e77f643625
ikeepcalm <horokhbohdandmytrovich@gmail.com>
2025-11-10 20:11:16 +0200

    Implement toString methods for Car and Truck classes

--- src/main/java/ro/ugal/aciee/vehicles/garage/impl/Car.java
+++ src/main/java/ro/ugal/aciee/vehicles/garage/impl/Car.java
@@ -126,4 +126,11 @@ public class Car extends RollingVehicle {
         return actions;
     }
 
+    @Override
+    public String toString() {
+        return "Car{" +
+               "isRadioOn=" + isRadioOn +
+               ", isStorageFull=" + isStorageFull +
+               '}';
+    }
 }
